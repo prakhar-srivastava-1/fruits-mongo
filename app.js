b0ba998010714fe801c763bf5bdf8175daedfcdb
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-02-17 07:37:01 +0530

    Insert 3 fruits

--- app.js
+++ app.js
@@ -13,13 +13,50 @@ const client = new MongoClient(url, {useNewUrlParser: true});
 const dbName = 'fruitsDB';
 
 // Use connect method to connect to the server
-  client.connect(function(err){
-    assert.equal(null, err);
+client.connect(function(err){
+  assert.equal(null, err);
 
-    console.log('Connected successfully to server');
-    const db = client.db(dbName);
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
 
+  // do Database ops here and close the connection
+  insertDocuments(db, function(){
     client.close();
-    // const collection = db.collection('documents');
   });
-  
+});
+
+// Create a function to insert documents in Database
+const insertDocuments = function(db, callback){
+  // Get the collection "fruits"
+  const collection = db.collection("fruits");
+  // Insert documents - automatically creates if doesn't exist
+  // insertMany(Array_of_documents, function_validate_using_assert)
+  collection.insertMany([
+    // document 1 - Mango
+    {
+      name: "Mango",
+      points: 5,
+      comment: "King of Fruits!"
+    },
+    // document 2 - Apple
+    {
+      name: "Apple",
+      points: 5,
+      comment: "Keeps the doctor away."
+    },
+    // document 3 - Banana
+    {
+      name: "Banana",
+      points: 3,
+      comment: "Meh!"
+    }
+  ],
+  // Validations
+  function(err, result){
+    assert.equal(null, err);
+    assert.equal(3, result.insertedCount);
+    // assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents.")
+    callback(result);
+  });
+};
